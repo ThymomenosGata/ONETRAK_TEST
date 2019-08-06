66bf3814bfc3358972515dde17f0ea2f61bd42f4
ThymomenosGata <kostaki97@mail.ru>
2019-08-06 18:50:08 +0300

    create adapter

---
+++ app/src/main/java/wordy/com/onetrak_test/Datas.java
@@ -0,0 +1,51 @@
+package wordy.com.onetrak_test;
+
+public class Datas {
+
+    private int aerobic;
+    private long date;
+    private int run;
+    private int walk;
+
+    public Datas(int aerobic, long date, int run, int walk) {
+        this.aerobic = aerobic;
+        this.date = date;
+        this.run = run;
+        this.walk = walk;
+    }
+
+    public int getAerobic() {
+        return aerobic;
+    }
+
+    public void setAerobic(int aerobic) {
+        this.aerobic = aerobic;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    public int getWalk() {
+        return walk;
+    }
+
+    public void setWalk(int walk) {
+        this.walk = walk;
+    }
+
+
+
+}
--- app/src/main/java/wordy/com/onetrak_test/RecycleViewAdapter.java
+++ app/src/main/java/wordy/com/onetrak_test/RecycleViewAdapter.java
@@ -1,27 +1,69 @@
 package wordy.com.onetrak_test;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-public class RecycleViewAdapter extends BaseAdapter {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder> {
+
+    private List<Datas> datas;
+    private LayoutInflater inflater;
+
+    public RecycleViewAdapter(Context context, List<Datas> datas) {
+        this.datas = datas;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
     @Override
-    public int getCount() {
-        return 0;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = inflater.inflate(R.layout.onetrak_item, viewGroup, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Datas data = datas.get(i);
+        viewHolder.runSteps.setText(data.getRun());
+        viewHolder.aerobicSteps.setText(data.getAerobic());
+        viewHolder.walkSteps.setText(data.getWalk());
+        viewHolder.countSteps.setText(data.getWalk() + data.getAerobic() + data.getRun());
+        viewHolder.thisDay.setText(getDate(data.getDate(), "dd.MM.yyyy"));
+
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount() {
+        return datas.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView runSteps, walkSteps, aerobicSteps, countSteps, thisDay;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            runSteps = itemView.findViewById(R.id.run);
+            walkSteps = itemView.findViewById(R.id.walk);
+            aerobicSteps = itemView.findViewById(R.id.aerobic);
+            countSteps = itemView.findViewById(R.id.count_steps);
+            thisDay = itemView.findViewById(R.id.date);
+        }
     }
+
+    private static String getDate(long milliSeconds, String dateFormat) {
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
+
 }
