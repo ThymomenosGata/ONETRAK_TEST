78b0759c9d7b7cf53fb1216ec57411109992b79f
ThymomenosGata <kostaki97@mail.ru>
2019-08-06 18:30:51 +0300

    init repos

---
+++ app/src/main/java/wordy/com/onetrak_test/MainActivity.java
@@ -0,0 +1,16 @@
+package wordy.com.onetrak_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
